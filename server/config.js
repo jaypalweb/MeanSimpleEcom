2e7e2617e93252bc7d60b7b32296bc41f679ad36
Jay Pal <contactjaypal@gmail.com>
2020-08-30 19:18:10 +0530

    connect with mongo db and aded CORS

---
+++ server/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    database: 'mongodb://localhost/simpleecom',
+    port: '3030',
+    secret: 'mysecret123'
+}
--- server/server.js
+++ server/server.js
@@ -2,13 +2,26 @@ const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cors = require('cors');
+
+const config = require('./config');
 
 const app = express();
+
+mongoose.connect(config.database, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
+.catch(error => console.error(error));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(morgan('dev'));
+app.use(cors());
+
+app.get('/', (req, res, next) => {
+res.json({
+    user: 'Jay'
+})
+});
 
 app.listen(3030, err => {
-    console.log('Magic happens on port 3030');
+    console.log('Magic happens on port awesome '+ config.port);
 });
