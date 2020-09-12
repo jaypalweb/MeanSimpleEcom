bb4b9c9647163443b8f7397eef5ca3bd5cf8e35d
Jay Pal <contactjaypal@gmail.com>
2020-09-13 00:47:45 +0530

    edit profile api

--- server/routes/account.js
+++ server/routes/account.js
@@ -93,6 +93,23 @@ router
       });
     });
   })
-  .post();
+  .post(checkJWT, (req, res, next) => {
+    User.findOne({ _id: req.decoded.user._id }, (err, user) => {
+      if (err) return next(err);
+
+      if (req.body.name) user.name = req.body.name;
+      if (req.body.email) user.email = req.body.email;
+      if (req.body.password) user.password = req.body.password;
+
+      user.isSeller = req.body.isSeller;
+
+      user.save();
+
+      res.json({
+        success: true,
+        message: 'Successfully edited your profile'
+      });
+    });
+  });
 
 module.exports = router;
